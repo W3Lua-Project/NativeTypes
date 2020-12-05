dc65fd7e43c4d3ca4aa0193a31861c99d1794492
WriteCoin <sovtsardom@gmail.com>
2020-12-05 11:22:01 +0300

    Класс приказов добавлен

--- id conversions.lua
+++ id conversions.lua
@@ -4,7 +4,9 @@ function fourCC(id)
     return FourCC(id)
 end
 
----@param id integer|string
+---@alias rawcode integer|string
+
+---@param id rawcode
 ---@return integer
 function formatIntCC(id)
     if type(id) == 'integer' then
@@ -20,13 +22,45 @@ function getStringCC(id)
     return ('>I4'):pack(id)
 end
 
----@param id integer|string
+---@param id rawcode
 ---@return string
 function formatStringCC(id)
     if type(id) == 'string' then
         return id
     elseif type(id) == 'integer' then
         return getStringCC(id)
+    end
+end
+
+---@class Rawcode
+RawCode = newClass()
+local class = RawCode
+
+---@param id rawcode
+---@return Rawcode
+function class:new(id)
+    self.id = formatIntCC(id)
+    self.stringId = formatStringCC(id)
+    return newObject(self)
+end
+
+---@param id Rawcode|rawcode
+---@return integer
+function class:formatInt(id)
+    if instanceof(id, class) then
+        return id.id
+    else
+        return formatIntCC(id)
+    end
+end
+
+---@param stringId Rawcode|rawcode
+---@return string
+function class:formatString(stringId)
+    if instanceof(stringId, class) then
+        return stringId.stringId
+    else
+        return formatStringCC(stringId)
     end
 end
 
@@ -39,10 +73,12 @@ end
 ---@param orderId integer
 ---@return string
 function getOrder2StringId(orderId)
-    return OrderId2String(formatIntCC(orderId))
+    return OrderId2String(orderId)
 end
 
----@param orderId integer|string
+---@alias order rawcode
+
+---@param orderId order
 ---@return integer
 function formatIntOrder(orderId)
     if type(orderId)=='integer' then
@@ -52,13 +88,45 @@ function formatIntOrder(orderId)
     end
 end
 
----@param order integer|string
+---@param order order
 ---@return string
 function formatStringOrder(order)
     if type(order)=='string' then
         return order
     elseif type(order)=='integer' then
         return getOrder2StringId(order)
+    end
+end
+
+---@class Order
+Order = newClass()
+local class = Order
+
+---@param id order
+---@return Order
+function class:new(id)
+    self.id = formatIntOrder(id)
+    self.stringId = formatStringOrder(id)
+    return newObject(self)
+end
+
+---@param id order|Order
+---@return integer
+function class:formatInt(id)
+    if instanceof(id, class) then
+        return id.id
+    else
+        return formatIntOrder(id)
+    end
+end
+
+---@param id order|Order
+---@return string
+function class:formatString(id)
+    if instanceof(id, class) then
+        return id.stringId
+    else
+        return formatStringOrder(id)
     end
 end
 
